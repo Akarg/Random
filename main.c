07ae1c76431193218aaa4a549ee061cc5e83118d
Adrian Cyran <acyran@linux-p110-13.inf.ug.edu.pl>
2018-11-03 10:16:27 +0100

    3/11

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
+// Wypisz przywitanie w kilku jezykach
+void hej(char* who);
+
 int main()
 {
-    printf("hello world\n");
+   hej("adrian");
+}
+
+// Funkcje pomocnicze
+
+void hej(char* who) 
+{
+        printf("hello %s\n", who);
+        printf("witaj %s\n", who);
 }
---
+++ main2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+  char who[6];
+  scanf("%s", who);
+
+  printf("hello %s\n", who);
+  printf("witaj %s\n", who);
+}
